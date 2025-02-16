c86d5c052478664635e59c30564bfdb4ccb434c9
CEMJ De Backer <cemjdebacker@telenet.be>
2025-02-16 20:01:30 +0100

    nieuwe welkom tekst!

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<p>Hello world!</p>"
+    return "<p>Welkom op mijn allereerste zelfgemaakte website!</p>"
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
